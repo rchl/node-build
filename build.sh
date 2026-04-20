023361f0df025e480efe76a93c3a09b7166e57d9
Rafal Chlodnicki <rchl2k@gmail.com>
2026-04-20 23:43:29 +0200

    add extra patch

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ NODE_TAG=v24.14.1
 git clone --depth 1 --branch "$NODE_TAG" https://github.com/nodejs/node.git node-src
 cd node-src
 git apply ../patches/support_v8_sandboxed_pointers.patch
+git apply ../patches/0001-apply-some-build-fixes-from-electron.patch
 ./configure \
     --ninja \
     --experimental-enable-pointer-compression \
